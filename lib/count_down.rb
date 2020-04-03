6dcad4cfc0e41a12f54f32f46338632ed4a42f9f
Oscar Oneim <oscar4londres@gmail.com>
2020-04-03 18:14:22 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -3,8 +3,8 @@ count = 10
 count_down = 0 
 
 while count > 0 do
-  puts "#{count}"
+  puts count
   count -= 1 
   if count == count_down do
-    puts "Happy New Year!"
 end
+puts "Happy New Year!"
